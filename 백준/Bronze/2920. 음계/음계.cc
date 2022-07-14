08f40e41ebe4327622eceb5f03049223437946ee
JuEun <86585240+kimjueun1@users.noreply.github.com>
2022-07-14 13:38:38 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str="";
+    getline(cin, str);
+    if(str=="1 2 3 4 5 6 7 8") cout << "ascending";
+    else if(str=="8 7 6 5 4 3 2 1") cout << "descending";
+    else cout << "mixed";
+    return 0;
+}
