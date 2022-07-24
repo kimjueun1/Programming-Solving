0be980f75b3ea0880afd2c1c83b54abccc8ad2c6
JuEun <86585240+kimjueun1@users.noreply.github.com>
2022-07-24 23:43:01 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+
+long long solution(int w,int h) {
+    long long answer = 0;
+    for(int i=1; i<w; i++){
+        long long n = ((double)h*i)/w;
+        answer += n;
+    }
+    return answer*2;
+}
