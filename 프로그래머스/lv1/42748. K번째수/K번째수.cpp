82987e88c6d0fd6ccbe9be86cf71e03b6d04e7d5
JuEun <86585240+kimjueun1@users.noreply.github.com>
2022-07-25 09:15:33 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    int cnt=0;
+    for(int i=0;i<commands.size();i++){
+        cnt=0;
+        vector<int> arr;
+        for(int j=commands[i][0]-1;j<commands[i][1];j++){
+            arr.push_back(array[j]);
+        }
+        sort(arr.begin(), arr.end());
+        answer.push_back(arr[commands[i][2]-1]);
+    }
+    return answer;
+}
